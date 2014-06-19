ce8c3ec75c03292d93f0de5e351fddb901754ce7
Max Patsiansky <m.patsianskiy@zmeke.com>
2014-06-19 17:20:19 +0400

    preparation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 var askerApp = angular.module('askerApp', ['ngAnimate']);
+
+askerApp.controller('mainCtrl', ['$scope', '$filter', '$timeout', '$http', function ($scope, $filter, $timeout, $http) {
+  $scope.test = 'qq';
+
+  $http.get('json/questions.json').success(function(data) {
+    $scope.questions = data;
+  });
+
+}]);
+
+
+askerApp.directive("questionRadio", function(){
+  return {
+    restrict: "EA",
+    transform: true,
+    templateUrl: "js/directives/templates/question-radio.html",
+
+    link: function(scope, element, attributes){
+      console.log(scope)
+    }
+    /*controller: function($scope){
+
+    }*/
+   };
+});
